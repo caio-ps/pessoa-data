fb9f9eeb88020f381ddab7a3330c27701fe98bdd
Caio Sousa <caio.psousa@fluig.com>
2013-09-30 11:54:04 -0300

    Adicionando estrutura de endereços

---
+++ src/main/java/pessoa/model/Endereco.java
@@ -0,0 +1,81 @@
+package pessoa.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "enderecos")
+public class Endereco extends Key {
+
+	private String logradouro;
+	private Long numero;
+	private String complemento;
+	private String cep;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	private String país;
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getPaís() {
+		return país;
+	}
+
+	public void setPaís(String país) {
+		this.país = país;
+	}
+	
+}
